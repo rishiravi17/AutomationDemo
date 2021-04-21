f622d7fbe5862fb37050127e6e66fc8db111ddcd
rishiravi17 <rishiravi17@gmail.com>
2021-04-21 06:10:55 +0000

    java demo project

---
+++ src/test/java/stepdefinitions/SurfBrowsersTest.java
@@ -0,0 +1,50 @@
+package stepdefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class SurfBrowsersTest {
+	
+	private WebDriver driver;
+	
+	@Given("^Initializing driver$")
+	public void initializing_driver() throws Throwable {
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/resources/drivers/chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Given("^The site to browse is \"([^\"]*)\"$")
+	public void the_site_to_browse_is(String siteUrl) {
+		
+		
+		driver.get(siteUrl);
+		
+		driver.manage().window().maximize();
+		
+		System.out.println("Browsing site: " + driver.getTitle());
+		
+		
+		
+	}
+
+	@Then("^User shall get the title \"([^\"]*)\"$")
+	public void user_shall_get_the_title(String title) {
+		
+		System.out.println("Asserting actual matching the expected.");
+		
+		Assert.assertEquals(driver.getTitle(), title);
+		
+		driver.quit();
+		
+	}
+	
+	
+
+}
---
+++ target/test-classes/features/Smoke.feature
@@ -0,0 +1,11 @@
+Feature: In order to surf different sites
+	As a user
+	I want to see the titles
+
+Background: Setup drivers
+Given Initializing driver
+
+@Smoke	
+Scenario: Surf different sites
+Given The site to browse is "https://the-internet.herokuapp.com/"
+Then User shall get the title "The Internet"
