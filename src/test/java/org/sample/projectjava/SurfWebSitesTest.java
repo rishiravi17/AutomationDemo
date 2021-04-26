5dbe9a655c8922dec05f6aa7187aaac3421c62e9
rishiravi17 <rishiravi17@gmail.com>
2021-04-26 08:23:29 +0000

    Removed ffox import

--- src/test/java/org/sample/projectjava/SurfWebSitesTest.java
+++ src/test/java/org/sample/projectjava/SurfWebSitesTest.java
@@ -2,7 +2,6 @@ package org.sample.projectjava;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
